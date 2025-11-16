b04d049d7c13767aa6400e69810179d127aae770
Jos Dehaes <jos.dehaes@gmail.com>
2025-11-17 00:31:52 +0100

    sanitize 0

--- lua/opts.lua
+++ lua/opts.lua
@@ -10,3 +10,5 @@ vim.opt.termguicolors = true
 vim.opt.undofile = true
 vim.opt.number = true
 vim.opt.clipboard:append({ "unnamed", "unnamedplus" })
+-- Remap 0 to go to first non-blank character
+vim.cmd([[nnoremap <expr> <silent> 0 col('.') == match(getline('.'),'\\S')+1 ? '0' : '^']])
