d4ee03948870e86b5982b9706f479122f07e2091
Jos Dehaes <jos.dehaes@gmail.com>
2025-11-17 12:36:22 +0100

    use mini for move and pairs

--- init.lua
+++ init.lua
@@ -2,6 +2,9 @@ require("opts")
 require("config.lazy")
 require("mappings")
 
+require("mini.move").setup()
+require("mini.pairs").setup()
+
 vim.lsp.enable("lua_ls", "bashls")
 
 vim.cmd("colorscheme vscode")
--- lua/plugins/all.lua
+++ lua/plugins/all.lua
@@ -2,5 +2,6 @@ return {
 	{ "Mofiqul/vscode.nvim" },
 	{ "tpope/vim-fugitive", lazy = false },
 	{ "j-hui/fidget.nvim", event = "LspAttach", opts = {} },
-	{ "nvim-mini/mini.nvim", version = "*" },
+	{ "nvim-mini/mini.move", version = "*" },
+	{ "nvim-mini/mini.pairs", version = "*" },
 }
