a5e57a49b0416536f37a2ef104b89312193363c6
Jos Dehaes <jos.dehaes@gmail.com>
2025-11-17 23:42:06 +0100

    some extra options

--- lua/opts.lua
+++ lua/opts.lua
@@ -11,6 +11,10 @@ vim.opt.smartindent = true
 vim.opt.termguicolors = true
 vim.opt.undofile = true
 vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 8
+vim.opt.mouse = "a"
 vim.opt.clipboard:append({ "unnamed", "unnamedplus" })
 -- Remap 0 to go to first non-blank character
 vim.cmd([[nnoremap <expr> <silent> 0 col('.') == match(getline('.'),'\\S')+1 ? '0' : '^']])
