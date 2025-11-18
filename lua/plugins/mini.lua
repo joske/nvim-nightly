db6b70f617efef26696b16c338f1a42531c5a0ed
Jos Dehaes <jos.dehaes@gmail.com>
2025-11-19 00:29:44 +0100

    split off all plugins

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,7 +1,4 @@
 return {
-    { "Mofiqul/vscode.nvim" },
-    { "tpope/vim-fugitive", lazy = false },
-    { "j-hui/fidget.nvim", event = "LspAttach", opts = {} },
     {
         "nvim-mini/mini.move",
         version = "*",
