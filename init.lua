be03b39ba1ab20d40e55c1dbfd09f3e461730e25
Jos Dehaes <jos.dehaes@gmail.com>
2025-11-18 14:33:47 +0100

    fix lsp enable syntax

--- init.lua
+++ init.lua
@@ -1,18 +1,18 @@
-require("opts")
-require("config.lazy")
-require("mappings")
+require "opts"
+require "config.lazy"
+require "mappings"
 
 require("mini.move").setup()
 require("mini.pairs").setup()
 
-vim.lsp.enable("lua_ls", "bashls", "yamlls", "tombi", "shellcheck")
+vim.lsp.enable { "lua_ls", "bashls", "yamlls", "tombi", "shellcheck", "xmlformatter", "texlab" }
 
-local null_ls = require("null-ls")
+local null_ls = require "null-ls"
 
-null_ls.setup({
-	sources = {
-		null_ls.builtins.formatting.shfmt,
-		null_ls.builtins.formatting.yamlfmt,
-	},
-})
-vim.cmd("colorscheme vscode")
+null_ls.setup {
+    sources = {
+        null_ls.builtins.formatting.shfmt,
+        null_ls.builtins.formatting.yamlfmt,
+    },
+}
+vim.cmd "colorscheme vscode"
