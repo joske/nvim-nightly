d0369fedabc9ba22bf1ac3b31d8b668f9586fd57
Jos Dehaes <jos.dehaes@gmail.com>
2025-11-24 22:39:52 +0100

    remove

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,9 +1,6 @@
 return {
     "L3MON4D3/LuaSnip",
     event = "InsertEnter",
-    build = vim.fn.has "win32" == 0
-            and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build\n'; make install_jsregexp"
-        or nil,
     dependencies = { "rafamadriz/friendly-snippets" },
     opts = {
         history = true,
