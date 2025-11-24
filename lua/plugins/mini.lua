31560d9d12bec414e9f3a97fb71e99bbc317aa59
Jos Dehaes <jos.dehaes@gmail.com>
2025-11-24 10:00:35 +0100

    replace some plugins with mini

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -9,4 +9,33 @@ return {
         version = "*",
         config = function() require("mini.pairs").setup() end,
     },
+    {
+        "nvim-mini/mini.clue",
+        version = "*",
+        config = function()
+            require("mini.clue").setup(
+                {
+                    -- Register `<Leader>` as trigger
+                    triggers = {
+                        { mode = 'n', keys = '<Leader>' },
+                        { mode = 'x', keys = '<Leader>' },
+                    },
+                }
+            )
+        end,
+    },
+    {
+        "nvim-mini/mini.comment",
+        version = "*",
+        config = function()
+            require("mini.comment").setup({
+                mappings = {
+                    comment = "<leader>/",
+                    comment_line = "<leader>/",
+                    comment_visual = "<leader>/",
+                    textobject = "<leader>/",
+                },
+            })
+        end,
+    },
 }
