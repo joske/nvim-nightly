36b8f96cde41553ff45bb5a21300e3d713a646dc
Jos Dehaes <jos.dehaes@gmail.com>
2026-04-09 22:23:42 +0200

    fix treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,9 +6,4 @@ return {
         ensure_installed = { "lua", "rust", "bash", "toml", "json", "yaml", "markdown" },
         highlight = { enable = true },
     },
-    config = function(_, opts)
-        -- suppress LSP “missing-fields” warnings
-        ---@diagnostic disable-next-line: missing-fields
-        require("nvim-treesitter.configs").setup(opts)
-    end,
 }
